bf91ad0d4a702e47f25972325459f268335c735d
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-09 19:44:37 +1000

    Initial Player Character Class

---
+++ Source/MythsAndLegends/Private/Characters/PlayerCharacter.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MythsAndLegends/Public/Characters/PlayerCharacter.h"
+
+APlayerCharacter::APlayerCharacter()
+{
+    
+}
---
+++ Source/MythsAndLegends/Public/Characters/PlayerCharacter.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MythsAndLegends/Public/Characters/BaseCharacter.h"
+#include "PlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHSANDLEGENDS_API APlayerCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+public:
+    APlayerCharacter();
+
+private:
+    
+};
