1c93cab8c5df7f11660485f2807aba015c747ad6
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-10 09:53:16 +1000

    Initial Player AI

---
+++ Source/MythsAndLegends/Public/Controllers/PlayerAiController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PlayerAiController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHSANDLEGENDS_API APlayerAiController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
