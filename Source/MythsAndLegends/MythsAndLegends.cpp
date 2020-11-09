56f8df6002d5145245bf77f592e173d6246e44ff
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-09 18:44:05 +1000

    Initial Commit
    Initial Commit

---
+++ Source/MythsAndLegends/MythsAndLegends.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MythsAndLegends.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MythsAndLegends, "MythsAndLegends" );
+
+DEFINE_LOG_CATEGORY(LogMythsAndLegends)
+ 
