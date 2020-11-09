c288040618859a3a7052dba6e1163ed855b3c5b3
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-09 19:33:29 +1000

    Setup Base Class

--- Source/MythsAndLegends/Public/Characters/BaseCharacter.h
+++ Source/MythsAndLegends/Public/Characters/BaseCharacter.h
@@ -14,16 +14,4 @@ class MYTHSANDLEGENDS_API ABaseCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ABaseCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 };
