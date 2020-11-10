77712c7e0d9ca98c05a19787eb8a1df9e86f541f
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-10 18:31:45 +1000

    Added Follow Enemy

---
+++ Source/MythsAndLegends/Public/Characters/NPC.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/BaseCharacter.h"
+#include "NPC.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHSANDLEGENDS_API ANPC : public ABaseCharacter
+{
+	GENERATED_BODY()
+public:
+    ANPC();
+};
