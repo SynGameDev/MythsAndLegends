1451d1576931da991ab35e1cd6e43d57a8c5d342
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-11 18:25:05 +1000

    Initial Script

---
+++ Source/MythsAndLegends/Public/UI/MainHUD.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYTHSANDLEGENDS_API UMainHUD : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+    UMainHUD();
+    
+private:
+    
+};
