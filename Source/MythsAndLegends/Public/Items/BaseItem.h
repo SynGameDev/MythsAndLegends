1a7e133e5e975545f9ffcec305e7e08a30e3d5fd
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-11 13:10:39 +1000

    Added Collider & Outline Component

--- Source/MythsAndLegends/Public/Items/BaseItem.h
+++ Source/MythsAndLegends/Public/Items/BaseItem.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
 #include "BaseItem.generated.h"
 
 UENUM()
@@ -25,17 +26,29 @@ public:
 	// Sets default values for this actor's properties
 	ABaseItem()
 	{
-		ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesg"));
+		// Create the objects
+		Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+		ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+		MeshOutline = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Outline Mesh"));
 
+		// Set the collider to be root component
+		RootComponent = Collider;
+		
+		// Add tags to this item
 		Tags.Add(FName("Item"));
 		Tags.Add(FName("Outline"));
 		Tags.Add(FName("Pickable"));
 	}
 
+	
 	FORCEINLINE UStaticMeshComponent* GetItemMesh() const { return ItemMesh; }
 protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
 	UStaticMeshComponent* ItemMesh;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+	UBoxComponent* Collider;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Components", meta=(AllowPrivateAccess="true"))
+	UStaticMeshComponent* MeshOutline;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Item Details", meta=(AllowProtectedAccess="true"))
 	FName ItemName;
