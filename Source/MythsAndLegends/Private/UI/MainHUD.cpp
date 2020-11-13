8fb35dbbd255327ff935f24b500af6619a00022b
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-13 14:47:57 +1000

    Added Player & Enemy Health bar controls

--- Source/MythsAndLegends/Private/UI/MainHUD.cpp
+++ Source/MythsAndLegends/Private/UI/MainHUD.cpp
@@ -2,8 +2,13 @@
 
 
 #include "MythsAndLegends/Public/UI/MainHUD.h"
+
+#include "Characters/BaseCharacter.h"
 #include "Components/SizeBox.h"
 #include "UMG/Public/Blueprint/WidgetTree.h"
+#include "Components/ProgressBar.h"
+
+#include "Kismet/GameplayStatics.h"
 
 UMainHUD::UMainHUD(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
@@ -12,21 +17,65 @@ UMainHUD::UMainHUD(const FObjectInitializer& ObjectInitializer) : Super(ObjectIn
 
 void UMainHUD::OpenInventory(APlayerController* Controller)
 {
-    UE_LOG(LogTemp, Warning, TEXT("Validating"));
     if(!InventoryWidgetClass && InventoryContainer)
         return;
 
-    UE_LOG(LogTemp, Warning, TEXT("Toggling"))
-
+    // Toggle The Inventory Variables
     IsInventoryOpen = !IsInventoryOpen;
+    // If the inventory variable is true, than create a Widget of the inventory & add it to the HUD
+    // Otherwise close the inventory & remove any inventory Widgets
     if(IsInventoryOpen)
     {
-        UE_LOG(LogTemp, Warning, TEXT("Opened"));
         InventoryWidget = WidgetTree->ConstructWidget<UWidget>(InventoryWidgetClass);
         InventoryContainer->AddChild(InventoryWidget);
     } else
     {
+        SelectedItemPoint->ClearChildren();
         InventoryContainer->RemoveChild(InventoryWidget);
         InventoryWidget = nullptr;
     }
+}
+
+void UMainHUD::SetupTimers()
+{
+    
+}
+
+void UMainHUD::SetEnemyTarget(USkillComponent* NewTarget)
+{
+    // Set the new target
+    TargetSkillComponent = NewTarget;
+    // Display the Enemy Health bar
+    EnemyHealthBar->SetVisibility(ESlateVisibility::Visible);
+}
+
+void UMainHUD::StartEnemyTargetTimer()
+{
+    // Start a timer, once it runs out it will remove the target
+    GetWorld()->GetTimerManager().SetTimer(ThEnemyTarget, this, &UMainHUD::UnsetEnemyTarget, 10.0f, false);
+}
+
+void UMainHUD::UnsetEnemyTarget()
+{
+    // Set the target to be null
+    TargetSkillComponent = nullptr;
+    // Hide the Progressbar
+    EnemyHealthBar->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UMainHUD::UpdatePlayerHealth()
+{
+    // Get the player character, get the skilll component & create the percentage
+    if(auto* const PlayerChar = Cast<ABaseCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0)))
+    {
+        PlayerHealth = PlayerChar->GetSkillComponent()->GetCharacterHealth() / 100;
+    }
+}
+
+void UMainHUD::UpdateEnemyHealth()
+{
+    if(!TargetSkillComponent)
+        return;
+
+    EnemyHealthBar->Percent = TargetSkillComponent->GetCharacterHealth() / 100;
 }
--- Source/MythsAndLegends/Public/UI/MainHUD.h
+++ Source/MythsAndLegends/Public/UI/MainHUD.h
@@ -18,25 +18,52 @@ class MYTHSANDLEGENDS_API UMainHUD : public UUserWidget
 public:
 
     UMainHUD(const FObjectInitializer& ObjectInitializer);
-    
+
+    // This will open the inventory
     UFUNCTION(BlueprintCallable)
     void OpenInventory(class APlayerController* Controller);
 
+    // Set's up any timers
+    UFUNCTION(BlueprintCallable)
+    void SetupTimers();
+
+    // --- Enemy Methods --- //
+    void SetEnemyTarget(class USkillComponent* NewTarget);
+    void StartEnemyTargetTimer();
+    void UnsetEnemyTarget();
+
+    // --- GETTERS --- //
     FORCEINLINE class USizeBox* GetSelectedItemPoint() const { return SelectedItemPoint; }
     FORCEINLINE UWidget* GetInventoryWidget() const { return InventoryWidget; }
-    
+
 private:
+    // --- UI OBJECTS --- //
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="UI Widgets", meta=(AllowPrivateAccess="true"))
     class USizeBox* InventoryContainer;
-
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Inventory|Variables", meta=(AllowPrivateAccess="true"))
     bool IsInventoryOpen = false;
-    
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Inventory|Variables", meta=(AllowPrivateAccess="true"))
     TSubclassOf<UUserWidget> InventoryWidgetClass;
     class UWidget* InventoryWidget;
-
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Inventory|Variables", meta=(AllowPrivateAccess="true"))
     class USizeBox* SelectedItemPoint;
+
+    // --- PLAYERS --- //
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Player|ProgressBasrs", meta=(AllowPrivateAccess="true"))
+    float PlayerHealth;
+    UFUNCTION(BlueprintCallable)
+    void UpdatePlayerHealth();
+
+    // --- ENEMY --- //
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Enemies", meta=(AllowPrivateAccess="true"))
+    class UProgressBar* EnemyHealthBar;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Enemies", meta=(AllowPrivateAccess="true"))
+    class USkillComponent* TargetSkillComponent;
+    UFUNCTION(BlueprintCallable)
+    void UpdateEnemyHealth();
+
+    // --- TIMERS --- //
+    FTimerHandle ThEnemyTarget;
+        
     
 };
