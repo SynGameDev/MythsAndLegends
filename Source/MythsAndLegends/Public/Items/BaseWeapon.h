a61c1ac512b77a62442d67084c01fa4d79b1f430
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-13 10:24:29 +1000

    Updated Values to have Protected Access & Added Socket attachment

--- Source/MythsAndLegends/Public/Items/BaseWeapon.h
+++ Source/MythsAndLegends/Public/Items/BaseWeapon.h
@@ -24,15 +24,22 @@ class MYTHSANDLEGENDS_API ABaseWeapon : public ABaseItem
 	GENERATED_BODY()
 public:
 	ABaseWeapon();
-private:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowPrivateAccess="true"))
+	FORCEINLINE float GetMinDamage() const { return MinDamage; }
+	FORCEINLINE float GetMaxDamage() const { return MaxDamage; }
+	FORCEINLINE UAnimMontage* GetAttackAnimation() const { return AttackMontages[FMath::RandRange(0, AttackMontages.Num())]; }
+	FORCEINLINE FName GetSocketName() const { return SocketName; }
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowProtectedAccess="true"))
 	float MinDamage;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowPrivateAccess="true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowProtectedAccess="true"))
 	float MaxDamage;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowPrivateAccess="true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowProtectedAccess="true"))
 	TArray<class UAnimMontage*> AttackMontages;
 	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowPrivateAccess="true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="WeaponDetails", meta=(AllowProtectedAccess="true"))
 	TEnumAsByte<EWeaponType> SwordType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Attachment|Socket", meta=(AllowPrivateAccess="true"))
+	FName SocketName;
 	
 };
