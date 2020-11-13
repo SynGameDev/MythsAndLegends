ec53ba30978cb74b04ac070cc62cc3d95153c545
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-14 00:14:51 +1000

    Added NPC Sight

--- Source/MythsAndLegends/Private/Characters/NPC.cpp
+++ Source/MythsAndLegends/Private/Characters/NPC.cpp
@@ -2,8 +2,35 @@
 
 
 #include "MythsAndLegends/Public/Characters/NPC.h"
+#include "Perception/PawnSensingComponent.h"
+#include "MythsAndLegends/Public/Controllers/BaseAIController.h"
+#include "MythsAndLegends/Public/Characters/PlayerCharacter.h"
 
 ANPC::ANPC()
 {
     PrimaryActorTick.bCanEverTick = true;
+
+    PawnSensingComponent = CreateDefaultSubobject<UPawnSensingComponent>(TEXT("Pawn Sense Component"));
+    PawnSensingComponent->SetPeripheralVisionAngle(90.0f);
+}
+
+void ANPC::BeginPlay()
+{
+    Super::BeginPlay();
+    if(PawnSensingComponent)
+    {
+        PawnSensingComponent->OnSeePawn.AddDynamic(this, &ANPC::OnSeePlayer);
+    }
+}
+
+void ANPC::OnSeePlayer(APawn* Pawn)
+{
+    UE_LOG(LogTemp, Warning, TEXT("SEEN PAWN"));
+    if(auto* const AI_Con = Cast<ABaseAIController>(GetController()))
+    {
+        if(auto* PlayerChar = Cast<APlayerCharacter>(Pawn))
+        {
+            AI_Con->SetOnSeenTarget(Pawn);
+        }
+    }
 }
