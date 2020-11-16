22ffae6199e9c91771cdd9b196fd384954aad506
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-16 16:28:19 +1000

    Created script to get the next path point

---
+++ Source/MythsAndLegends/Private/AI_Task/IncrementPathIndex.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MythsAndLegends/Public/AI_Task/IncrementPathIndex.h"
+
+#include <string>
+
+
+#include "MythsAndLegends/Public/Characters/NPC.h"
+#include "MythsAndLegends/Public/Controllers/BaseAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UIncrementPathIndex::UIncrementPathIndex()
+{
+    NodeName = "Increment Path Index";
+}
+
+EBTNodeResult::Type UIncrementPathIndex::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+    if(auto* const NPC_Con = Cast<ABaseAIController>(OwnerComp.GetOwner()))
+    {
+        int32 CurrentIndex = NPC_Con->GetBlackboardComponent()->GetValueAsInt(NPC_Con->Key_TargetIndex);
+
+        if(auto* const NPC_Char = Cast<ANPC>(NPC_Con->GetPawn()))
+        {
+            if((NPC_Char->GetPatrolPoints()->GetTargetPoints().Num() - 1) == CurrentIndex)
+            {
+                CurrentIndex = 0;
+            } else
+            {
+                CurrentIndex++;
+            }
+
+            NPC_Con->GetBlackboardComponent()->SetValueAsInt(NPC_Con->Key_TargetIndex, CurrentIndex);
+            UE_LOG(LogTemp, Warning, TEXT("Incremented Path: %i"), CurrentIndex);
+        }
+    }
+
+    FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+    return EBTNodeResult::Succeeded;
+}
