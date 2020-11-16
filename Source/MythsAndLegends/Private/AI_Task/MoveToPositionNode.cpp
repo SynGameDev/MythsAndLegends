533b5f65eb5f917eb022b946aa1735e48ba9d12c
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-11-16 16:29:17 +1000

    Added Script to get & set the next target location

--- Source/MythsAndLegends/Private/AI_Task/MoveToPositionNode.cpp
+++ Source/MythsAndLegends/Private/AI_Task/MoveToPositionNode.cpp
@@ -6,12 +6,36 @@
 #include "BehaviorTree/BlackboardComponent.h"
 #include "Characters/ML_PlayerController.h"
 #include "Characters/PlayerCharacter.h"
+#include "MythsAndLegends/Public/Characters/BaseCharacter.h"
+#include "MythsAndLegends/Public/Characters/NPC.h"
+#include "MythsAndLegends/Public/AI_Task/AITargetPoint.h"
 
 UMoveToPositionNode::UMoveToPositionNode()
 {
+   NodeName = "Targeted Location";
 }
 
 EBTNodeResult::Type UMoveToPositionNode::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+   UE_LOG(LogTemp, Warning, TEXT("Starting Task"));
+   if(auto* const NPC_Con = Cast<ABaseAIController>(OwnerComp.GetOwner()))
+   {
+      UE_LOG(LogTemp, Warning, TEXT("Got Controller"));
+      if(auto* const NPC_Char = Cast<ANPC>(NPC_Con->GetPawn()))
+      {
+         UE_LOG(LogTemp, Warning, TEXT("Got Pawn"));
+         // Get the current target index
+         int32 const TargetIndex = NPC_Con->GetBlackboardComponent()->GetValueAsInt(NPC_Con->Key_TargetIndex);
+         // Get the location of that target point & Convert it to world space
+         FVector const TargetLocation = NPC_Char->GetPatrolPoints()->GetTargetPointPosition(TargetIndex);
+         FVector const WorldTargetLocation = NPC_Char->GetPatrolPoints()->GetActorLocation() + TargetLocation;
+
+         
+         UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *TargetLocation.ToString());
+
+         NPC_Con->GetBlackboardComponent()->SetValueAsVector(NPC_Con->Key_TargetLocation, WorldTargetLocation);
+      }
+   }
+   FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
    return EBTNodeResult::Succeeded;
 }
