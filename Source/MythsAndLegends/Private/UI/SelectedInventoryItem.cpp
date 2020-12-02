3c682e0c1d3af751bbf5cce6178981d5c39f7ae3
Keegan Miller <Keegan.Miller@syndicate-games.com>
2020-12-03 02:15:08 +1000

    Setup Values

--- Source/MythsAndLegends/Private/UI/SelectedInventoryItem.cpp
+++ Source/MythsAndLegends/Private/UI/SelectedInventoryItem.cpp
@@ -7,26 +7,29 @@
 #include "MythsAndLegends/Public/Characters/InventoryComponent.h"
 #include "Characters/PlayerCharacter.h"
 #include "Kismet/GameplayStatics.h"
-
+#include "Components/TextBlock.h"
 void USelectedInventoryItem::SetValues(FName const Name, FText const Desc)
 {
-    ItemName = Name;
-    ItemDesc = Desc;
+    ItemNameBlock->SetText(FText::FromName(Name));
+    ItemDescription->SetText(Desc);
+    
+
+    
 }
 
 void USelectedInventoryItem::EquipItem()
 {
-    UE_LOG(LogTemp, Warning, TEXT("1"));
+    
     if(auto* const PlayerChar = Cast<APlayerCharacter>(UGameplayStatics::GetPlayerCharacter(
         this, 0)))
     {
-        UE_LOG(LogTemp, Warning, TEXT("2"));
+        
         if(ABaseItem* const item = Cast<UInventoryUI>(InventoryWidget)->GetSelectedItem())
         {
-            UE_LOG(LogTemp, Warning, TEXT("3"));
+           
             if(auto* const Inv = PlayerChar->GetInventoryComponent())
             {
-                UE_LOG(LogTemp, Warning, TEXT("4"));
+                
                 Inv->EquipItem(item);
             }
         }
